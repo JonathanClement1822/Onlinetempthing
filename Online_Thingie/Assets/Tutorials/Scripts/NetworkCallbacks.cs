7ebd35f7d9e61e761d07b382b23847ff78cad6e5
Chris Medina <Chrismed68@gmail.com>
2020-05-02 21:24:21 -0400

    tutorial 2 is done and working

---
+++ Online_Thingie/Assets/Tutorials/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using Bolt;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : GlobalEventListener
+{
+  public override void SceneLoadLocalDone(string scene)
+  {
+    // Your code here...
+
+    //randomize a position
+    var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
+
+    //instantiate cube
+    BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+  }
+}
